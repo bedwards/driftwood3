043d873a3cb2fc58cc5c5c2ca8e2f63bd18b00db
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-09-17 13:09:21 -0500

    version 4 - take 1

--- server-4.py
+++ server-4.py
@@ -100,10 +100,10 @@ async def main():
                 handler,
                 "0.0.0.0",
                 port,
-                ping_interval=30,     # Send ping every 20 seconds
-                ping_timeout=20,      # Wait 10 seconds for pong
+                ping_interval=30,     # Send ping every n seconds
+                ping_timeout=300,     # Wait n seconds for pong
                 close_timeout=15,     # Time to wait for clean close
-                max_size=1048576,     # 1MB message limit
+                max_size=1048576,     # n MB message limit
                 max_queue=32,         # Limit queued messages
                 compression=None      # Disable compression for lower latency
             ):
