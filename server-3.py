2b4b963769030ae2d65f0e9bfcff24746e3e1471
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-09-17 09:10:16 -0500

    male/female voice fix 2

--- server-3.py
+++ server-3.py
@@ -31,8 +31,16 @@ assert DEVICE == "mps"
 # tts = TTS("tts_models/multilingual/multi-dataset/your_tts").to(DEVICE)
 tts = TTS("tts_models/en/vctk/fast_pitch").to(DEVICE)
 
+speakers = tts.list_speaker_idxs()
+print(speakers)
+
 SR = getattr(getattr(tts, "synthesizer", None), "output_sample_rate", 22050)
-FEMALE, MALE = "p225", "p226"
+
+
+# Male: p225, p228, p236
+# Female: p262, p270, p315
+FEMALE, MALE = "p315", "p236"
+
 turn = {"n": 0}
 SENT = re.compile(r"([^.?!\n]+[.?!\n]+)")
 
